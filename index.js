397f4ece734720c1f693edefe60c2403e5440c1e
Valeriia-Ro <vkolchak17@gmail.com>
2021-11-24 18:53:11 +0200

    have problem with remove

---
+++ index.js
@@ -0,0 +1,36 @@
+const {
+  listContacts,
+  getContactById,
+  removeContact,
+  addContact,
+  contactsName,
+} = require("./contacts.js");
+
+const argv = require("yargs").argv;
+
+function invokeAction({ action, id, name, email, phone }) {
+  switch (action) {
+    case "list":
+      listContacts();
+      break;
+
+    case "get":
+      getContactById(id);
+      break;
+
+    case "add":
+      addContact(name, email, phone);
+      break;
+
+    case "remove":
+      removeContact(id);
+      break;
+
+    default:
+      console.warn("\x1B[31m Unknown action type!");
+  }
+}
+
+(async () => {
+  await invokeAction(argv);
+})();
